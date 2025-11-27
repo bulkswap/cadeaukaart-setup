fcf245da8db39af29bacfec598cd1e1826690d4e
bulkswap <easyclickmediainfo@gmail.com>
2025-11-27 05:13:10 +0100

    Update open.js

--- api/open.js
+++ api/open.js
@@ -1,1 +1,11 @@
+// api/open.js
+export default function handler(req, res) {
+  const { id } = req.query;
+  const data = globalThis[id];
 
+  if (!data || Date.now() > data.expires) {
+    return res.status(410).json({error: "expired"});
+  }
+
+  res.json(data);
+}
